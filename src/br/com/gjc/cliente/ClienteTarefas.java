19fbb4525f6ad1032ac086d94a7cb17cbc180858
welnovaesm <wel.miro@hotmail.com>
2022-02-09 09:54:02 -0300

    melhorando cliente

--- src/br/com/gjc/cliente/ClienteTarefas.java
+++ src/br/com/gjc/cliente/ClienteTarefas.java
@@ -1,5 +1,6 @@
 package br.com.gjc.cliente;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
@@ -10,14 +11,51 @@ public class ClienteTarefas {
 
         System.out.println("Conexao estabelecida");
 
-        PrintStream saida = new PrintStream(socket.getOutputStream());
-        saida.println("c1");
+        Thread threadEnviaComando = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    PrintStream saida = new PrintStream(socket.getOutputStream());
+                    Scanner scan = new Scanner(System.in);
+                    while (scan.hasNextLine()) {
+                        String linha = scan.nextLine();
 
-        Scanner scan = new Scanner(System.in);
-        scan.hasNextLine();
+                        if (linha.trim().equals("")) {
+                            break;
+                        }
+                        saida.println(linha);
+                    }
+                    saida.close();
+                    scan.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
 
-        saida.close();
-        scan.close();
+        Thread threadRecebeResposta = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Recebendo dados do servidor");
+                    Scanner respostaServidor = new Scanner(socket.getInputStream());
+                    while (respostaServidor.hasNextLine()) {
+                        String linha = respostaServidor.nextLine();
+                        System.out.println(linha);
+                    }
+                    respostaServidor.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        threadRecebeResposta.start();
+        threadEnviaComando.start();
+
+        threadEnviaComando.join();
+
+        System.out.println("Fechando socket do cliente");
         socket.close();
     }
 }
--- src/br/com/gjc/servidor/DistribuirTarefas.java
+++ src/br/com/gjc/servidor/DistribuirTarefas.java
@@ -1,5 +1,6 @@
 package br.com.gjc.servidor;
 
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -13,16 +14,28 @@ public class DistribuirTarefas implements Runnable {
 
     @Override
     public void run() {
-
         try {
             System.out.println("Distribuindo tarefas para " + socket);
             Scanner scan = new Scanner(socket.getInputStream());
+            PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
 
-            while(scan.hasNextLine()){
+            while (scan.hasNextLine()) {
                 String comando = scan.nextLine();
+                System.out.println("Comando recebido " + comando);
+
+                switch (comando) {
+                    case "c1":
+                        saidaCliente.println("Confirmação do comando c1");
+                        break;
+                    case "c2":
+                        saidaCliente.println("Confirmação do comando c2");
+                        break;
+                    default:
+                        saidaCliente.println("Comando não encontrado ");
+                }
                 System.out.println(comando);
             }
-
+            saidaCliente.close();
             scan.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
